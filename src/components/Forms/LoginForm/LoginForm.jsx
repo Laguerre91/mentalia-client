125e4ed19ffe1a649a651ba4142e6f3d333429fe
Lara Aguerre <aguerre.lara@gmail.com>
2024-03-04 19:00:41 +0100

    signup activated

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,14 +1,74 @@
 import { useContext, useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./../../../context/auth.context";
 
 const API_URL = "http://localhost:5005"
 
 const LoginForm = () => {
 
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const [errorMessage, setErrorMessage] = useState(undefined)
+
+    const navigate = useNavigate();
+
+    const handleInputChange = e => {
+        const { name, value } = e.target
+        setUserData((prevUserData) => ({
+            ...prevUserData,
+            [name]: value
+        }))
+    }
+
+    const { storeToken, authenticateUser } = useContext(AuthContext)
+
+    const handleLoginSubmit = (e) => {
+
+        e.preventDefault()
+
+        const requestBody = { email, password }
+
+        axios
+            .post(`${API_URL}/api/auth/login`, requestBody)
+            .then((response) => {
+                storeToken(response.data.authToken)
+                authenticateUser()
+                navigate('/')
+            })
+            .catch((error) => {
+                // const errorDescription = error.response.data.message;
+                // setErrorMessage(errorDescription);
+            })
+    }
+
+
+
     return (
         <div className="LoginForm">
+            <form onSubmit={handleLoginSubmit}>
+                <label>Email:</label>
+                <input
+                    type="email"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleInputChange}
+                />
+
+                <label>Password:</label>
+                <input
+                    type="password"
+                    name="password"
+                    value={userData.password}
+                    onChange={handleInputChange}
+                />
+
+                <button type="submit">Login</button>
+            </form>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
 
         </div>
     )
