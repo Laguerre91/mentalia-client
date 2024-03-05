d1c151a5b3bdbde36cf594c30dc2bdc8a5888793
Fran Pazos <franciscopazosrey@gmail.com>
2024-03-05 13:19:19 +0100

    Created modal with multi step form in it. And prepared for animation creating folder and file

---
+++ src/components/Animations/MoodAnimation.jsx
@@ -0,0 +1,9 @@
+const MoodAnimation = () => {
+
+    return (
+
+        <h1>Esto es una animación super chuli del Mood  :·3 </h1>
+    )
+}
+
+export default MoodAnimation
--- src/pages/UserDashboardPage.jsx
+++ src/pages/UserDashboardPage.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react"
+import { Button, Modal } from "react-bootstrap"
+import RecordForm from "../components/Forms/RecordForm/RecordForm"
+
 const UserDashboardPage = () => {
+
+    const [show, setShow] = useState(false)
+
 
     return (
         <div className="UserDashboardPage">
+
+            <h1>Hola User!</h1>
+
+            <Button variant="primary" onClick={() => setShow(true)}>
+                Mood Diario
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={() => setShow(false)}
+                dialogClassName="modal-90w"
+                aria-labelledby="example-custom-modal-styling-title"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-custom-modal-styling-title">
+                        COMO TE SIENTES HOY?
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                    <RecordForm />
+
+                </Modal.Body>
+            </Modal>
 
         </div>
     )
