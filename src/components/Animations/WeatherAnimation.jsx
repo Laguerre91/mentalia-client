9170784fad742f5ec69eb15f494152771b7311dd
Fran Pazos <franciscopazosrey@gmail.com>
2024-03-13 00:17:34 +0100

    added all svg weather animations, and applied functionalities in the recordform

--- src/components/Animations/WeatherAnimation.jsx
+++ src/components/Animations/WeatherAnimation.jsx
@@ -1,9 +1,37 @@
-const WeatherAnimation = () => {
+import storm from "../../assets/thunder.svg"
+import cloudy from "../../assets/cloudy.svg"
+import snowy from "../../assets/snowy-6.svg"
+import sunny from "../../assets/day.svg"
+import rainy from "../../assets/rainy-6.svg"
+
+const WeatherAnimation = ({ weatherValue }) => {
+    let weatherImage
+
+    switch (weatherValue) {
+        case 'Sol':
+            weatherImage = sunny
+            break
+        case 'Nubes':
+            weatherImage = cloudy
+            break
+        case 'Lluvia':
+            weatherImage = rainy
+            break
+        case 'Tormenta':
+            weatherImage = storm
+            break
+        case 'Nieve':
+            weatherImage = snowy
+            break
+        default:
+            weatherImage = null
+            break
+    }
 
     return (
-
-        <h1>Esto es una animación super chuli del Weather  :·3 </h1>
-
+        <div className="weather-icons">
+            <img src={weatherImage} alt={weatherValue} />
+        </div>
     )
 }
 
