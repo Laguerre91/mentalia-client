4a6ffffae96ba844d2d77c85a4052a81625807bb
Fran Pazos <franciscopazosrey@gmail.com>
2024-03-14 00:44:51 +0100

    added moodchart, changed svg of 'muy bien

--- src/components/Charts/MoodChart/MoodChart.jsx
+++ src/components/Charts/MoodChart/MoodChart.jsx
@@ -1,71 +1,81 @@
-import './MoodChart.css'
-
-import React, { useEffect, useState } from 'react';
-import { Chart } from 'react-google-charts';
-import recordServices from '../../../services/record.services';
+import React, { useEffect, useState } from 'react'
+import { Chart } from 'react-google-charts'
+import { useParams } from 'react-router-dom'
+import UserService from '../../../services/user.services'
 
 const MoodChart = () => {
-    const [chartData, setChartData] = useState([]);
+    const { userId } = useParams()
+    const [chartData, setChartData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        const getUserRecords = () => {
-            recordServices.getAllRecords()
+        const fetchMoodData = () => {
+
+            UserService.getUser(userId)
                 .then(response => {
-                    const allRecords = response.data;
-                    const moodCounts = {
-                        'Muy mal': 2,
-                        'Mal': 5,
-                        'Normal': 3,
-                        'Bien': 7,
-                        'Muy bien': 7
-                    };
+                    const allRecords = response.data.records
+
+                    const moodCount = {
+                        'Muy mal': 0,
+                        Mal: 0,
+                        Normal: 0,
+                        Bien: 0,
+                        'Muy bien': 0,
+                    }
 
                     allRecords.forEach(record => {
-                        moodCounts[record.moods] += 1;
-                    });
+                        moodCount[record.mood]++
+                    })
 
-                    const data = Object.entries(moodCounts).map(([mood, count]) => [mood, count]);
+                    const data = [['Mood', 'Count']]
+                    for (const mood in moodCount) {
+                        data.push([mood, moodCount[mood]])
+                    }
 
-                    setChartData([['Mood', 'Count'], ...data]);
+                    setChartData(data)
+                    setLoading(false)
                 })
                 .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        };
+                    console.error('Error fetching mood data:', error)
+                    setLoading(false)
+                })
+        }
 
-        getUserRecords();
-    }, []);
+        fetchMoodData()
+    }, [userId])
 
     return (
         <div>
             <h2 className='title'>Moods</h2>
-            <Chart
-                width={'100%'}
-                height={'200px'}
-                chartType="PieChart"
-                loader={<div>Loading Chart</div>}
-                data={chartData}
-                options={{
-                    pieHole: 0.5,
-                    slices: {
-                        0: { color: '#F7A4A6' }, // Muy mal
-                        1: { color: '#AFE0E9' }, // Mal
-                        2: { color: '#FCE573' }, // Normal
-                        3: { color: '#F7B1CB' }, // Bien
-                        4: { color: '#98D2AD' }  // Muy bien
-                    },
-                    backgroundColor: 'transparent',
-                    pieSliceTextStyle: {
-                        fontSize: 20
-                    },
-                    chartArea: {
-                        width: '100 %',
-                        height: '80%'
-                    }
-                }}
-            />
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <Chart
+                    width={'100%'}
+                    height={'200px'}
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={chartData}
+                    options={{
+                        pieHole: 0.5,
+                        slices: {
+                            0: { color: '#F7A4A6' }, // Muy mal
+                            1: { color: '#AFE0E9' }, // Mal
+                            2: { color: '#FCE573' }, // Normal
+                            3: { color: '#F7B1CB' }, // Bien
+                            4: { color: '#98D2AD' }  // Muy bien
+                        },
+                        backgroundColor: 'transparent',
+                        pieSliceTextStyle: {},
+                        chartArea: {
+                            width: '100 %',
+                            height: '80%'
+                        }
+                    }}
+                />
+            )}
         </div>
-    );
-};
+    )
+}
 
-export default MoodChart;
+export default MoodChart
